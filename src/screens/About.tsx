904095c219bcaec5bf9c1379c762614c9d87d86f
braxton hancock <bdhancock@olivet.edu>
2022-07-24 21:02:35 -0400

    successful navigation

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+import {useNavigate} from 'react-router-native';
 
 const About = () => {
+  let navigate = useNavigate();
+
   return (
     <View>
-      <Text>About Screen</Text>;<Button title="to about"></Button>
+      <Text>About Screen</Text>
+      <Button
+        title="to home"
+        onPress={() => {
+          navigate('/');
+        }}
+      />
     </View>
   );
 };
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+import {useNavigate} from 'react-router-native';
 
 const HomeScreen = () => {
+  let navigate = useNavigate();
+
   return (
     <View>
       <Text>Home Screen</Text>
-      <Button title="to about"></Button>
+      <Button
+        title="to about"
+        onPress={() => {
+          navigate('about');
+        }}
+      />
     </View>
   );
 };
